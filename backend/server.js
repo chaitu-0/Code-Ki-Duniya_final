9d9d3e04b6a11b366abc88f3004b5a4a1649d6f2
Chaitanya <bhavarrudra42@gmail.com>
2025-03-17 16:38:42 +0000

    helth check

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,12 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/posts', require('./routes/postRoutes')); // If you have post routes
 
+// helth check of render
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ message: "Backend is working!" });
+});
+
+
 // Server Listening
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
