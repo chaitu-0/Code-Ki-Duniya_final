a739dbfafb99d630a278d0ff29548f2c382daccf
Chaitanya <bhavarrudra42@gmail.com>
2025-03-17 07:24:05 +0000

    username unique remove

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    username: { type: String, required: true, unique: true }, // 🔥 Ensure it's required
+    username: { type: String, required: true }, // 🔥 Ensure it's required
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
